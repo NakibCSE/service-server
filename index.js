1e1d2371d5c918a03069abaadcd9f2e4d665da73
Nakib <nakkibcse@gmail.com>
2022-11-11 16:59:57 +0600

    json added

--- index.js
+++ index.js
@@ -15,20 +15,7 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-function verifyJwt(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
-    if (err) {
-      return res.status(401).send({ message: "unauthorized access" });
-    }
-    req.decoded = decoded;
-    next();
-  });
-}
+
 
 async function run() {
   try {
